ff397a433d12c278b1c6d2d7f393270703a05f6d
CosimoTramonte <cosimotramonte7@gmail.com>
2023-05-18 16:23:19 +0200

    add function of add new task

--- main.js
+++ main.js
@@ -5,15 +5,35 @@ createApp({
     return {
       apiUrl: "server.php",
       list: [],
+      newTask:"",
     }
   },
   methods:{
+
     getApi(){
       axios.get(this.apiUrl)
         .then(result => {
           this.list = result.data
         })
+    },
+
+    addNewTask(){
+      const data = new FormData()
+      data.append("taskText",this.newTask)
+      data.append("isDone", false)
+
+      axios.post(this.apiUrl, data)
+        .then(result =>{
+          this.newTask = "",
+          this.list = result.data
+          console.log("ecco la nuova lista");
+        })
+    },
+
+    removeTask(){
+      console.log("rimosso");
     }
+
   },
   mounted(){
     this.getApi()
--- server.php
+++ server.php
@@ -4,7 +4,29 @@ $json_string = file_get_contents("toDo-list.json");
 
 $list = json_decode($json_string, true);
 
+if(isset($_POST["taskText"])){
+
+    $newTask = [
+        "taskText" => $_POST["taskText"],
+        "isDone" => false
+    ];
+
+    $list[] = $newTask;
+    filePutContents($list);
+}
+
 header("Content-Type: application/json");
 
 echo json_encode($list);
+
+
+
+
+
+
+//---------------FUNCTIONS-----------------
+
+function filePutContents($list){
+    file_put_contents("toDo-list.json", json_encode($list));
+}
 ?>
